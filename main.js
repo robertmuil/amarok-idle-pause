66f264ea4925e66833b2cffe6f6b3f779e4f4035
Robert Muil <robert.muil@zalando.de>
2015-11-16 13:59:16 +0100

    working! does what i wantgit add main.js

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ Importer.loadQtBinding( "qt.uitools" );
 
 Amarok.debug("script started");
 
+SecondsUntilPause = 2*60*60; //1 hour
+
+secondsUntilPause = SecondsUntilPause;
+
 function POIDbgWin()
 {
 	QMainWindow.call(this, null);
   var mainWidget = new QWidget( this );
 	this.dbgList = new QListWidget(mainWidget);
+
+//	this.dbgList.rowsInserted.connect(this.dbgList.scrollToBottom);
 
 	var explanationItem = new QListWidgetItem("This is just a debug console.");
   explanationItem.setFlags( !Qt.ItemIsSelectable );
@@ -38,19 +44,96 @@ function dbg(dbg_txt) {
 	aa = new QListWidgetItem(dbg_txt);
 
 	dbgWin.dbgList.addItem(aa);
+	dbgWin.dbgList.scrollToBottom();
 
-	Amarok.Window.Statusbar.longMessage("POIDbg: " + dbg_txt);
+	Amarok.Window.Statusbar.shortMessage("POIDbg: " + dbg_txt);
 
+}
+
+function timedAlert(interval) {
+	var qo = new QTimer();
+	qo.timeout.connect(function(qevent) {
+		secondsUntilPause -= 1;
+		msg = 'Time-out: ' + secondsUntilPause + ' seconds until pause.';
+		dbg(msg)
+
+		if (secondsUntilPause <= 0) {
+			Amarok.Engine.Pause();
+			msg = 'Have been idle for '+SecondsUntilPause+' seconds: playback paused.';
+			dbg(msg);
+			Amarok.alert(msg);
+			qo.stop(); //shouldn' this be 'this' or something?
+		}
+
+		return true;
+	})
+	qo.start(interval);
+
+	return qo
+}
+
+function resetTimer() {
+	secondsUntilPause = SecondsUntilPause;
+	timerObj.start()
 }
 
 function tracks()
 {
 	dbg(Amarok.Playlist.totalTrackCount() + " tracks in playlist");
+	resetTimer();
 }
 
 Amarok.Playlist.trackInserted.connect(tracks);
 Amarok.Playlist.trackRemoved.connect(tracks);
 
+currentSeekPos = 0;
+Amarok.Engine.trackPlayPause.connect(function(isPause) {
+	//the trackPlayPause event is called whenever the engine pauses or plays,
+	//including when the track advances automatically to the next in the playlist.
+	//ie it's not a good indication of user activity.
+	msg = 'trackPlayPause event: ' + isPause + ': '
+	if (isPause) {
+		msg += 'pause';
+		//this should only occur on manual action or on our own action
+		timerObj.stop()
+	} else {
+		//this occurs also when the track changes in the playlist
+		msg+='play';
+
+		timerIsRunning = timerObj.active;
+		if (!timerIsRunning) {
+			dbg('timer was not running, activating.')
+			resetTimer();
+		}
+	}
+	dbg(msg);
+	return true;
+});
+
+Amarok.Engine.trackChanged.connect(function() {
+	currentSeekPos = 0;
+	dbg('trackChanged()');
+});
+
+Amarok.Engine.trackSeeked.connect(function(seekPos) {
+	//This is fired for ANY movement of a track, even just normal playing.
+	//so this will be useful for idle detection only if we maintain a counter
+	//and detect non-normal playback
+	if (seekPos < currentSeekPos) {
+		dbg('trackSeeked('+seekPos+') went backward');
+		resetTimer();
+	}
+	currentSeekPos = seekPos;
+});
+Amarok.Engine.volumeChanged.connect(function(vol) {
+	dbg('volumeChanged('+vol+')');
+	resetTimer();
+});
+Amarok.Engine.trackFinished.connect(function() {
+	// Doesn't seem to fire when track reaches end and next is immediately played
+	dbg('trackFinished()');
+});
+
 dbg("Ok, we've started!")
 
-
+timerObj = timedAlert(1000);
--- script.spec
+++ script.spec
@@ -3,14 +3,14 @@ Icon=preferences-other-amarok
 Type=script
 ServiceTypes=KPluginInfo
 
-Name=Amarok Pause on Idle
-Comment=Amarok Pause on Idle
+Name=Pause on Idle
+Comment=Pause on Idle script for Amarok
 
-X-KDE-PluginInfo-Name=Amarok Pause on Idle
+X-KDE-PluginInfo-Name=Pause on Idle
 X-KDE-PluginInfo-Version=0.1
 X-KDE-PluginInfo-Category=Generic
 X-KDE-PluginInfo-Website=https://github.com/robertmuil/amarok-idle-pause
 X-KDE-PluginInfo-Depends=Amarok2.0
 X-KDE-PluginInfo-License=GPL
 X-KDE-PluginInfo-BugAddress=<submit issue to github please>
-X-KDE-PluginInfo-EnabledByDefault=false
+X-KDE-PluginInfo-EnabledByDefault=true
